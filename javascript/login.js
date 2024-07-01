ff85dda5282298cbde5539b847d3701cc2d74d6e
Javier Mugnani <javieralejandroberzero@gmail.com>
2024-07-01 15:02:06 -0300

    tercera preentrega

--- javascript/login.js
+++ javascript/login.js
@@ -1,26 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
     const registerForm = document.getElementById("registerForm");
     const loginForm = document.getElementById("loginForm");
-  
+    
     registerForm.addEventListener("submit", function(event) {
         event.preventDefault();
 
         const email = document.getElementById("regEmail").value;
         const password = document.getElementById("regPassword").value;
 
-        
-        const passwordHash = btoa(password); 
+     
+        const passwordHash = btoa(password);
 
-
         
-        localStorage.setItem("email", email);
-        localStorage.setItem("passwordHash", passwordHash);
-        Swal.fire({
-            title: "Good job!",
-            text: "Registro exitoso. Ahora puedes iniciar sesión.!",
-            icon: "success"
-          });
-   
+        const user = {
+            email: email,
+            passwordHash: passwordHash
+        };
+
+       
+        localStorage.setItem("user", JSON.stringify(user));
+
+        alert("Registro exitoso. Ahora puedes iniciar sesión.");
     });
 
     loginForm.addEventListener("submit", function(event) {
@@ -29,19 +29,15 @@ document.addEventListener("DOMContentLoaded", function() {
         const email = document.getElementById("loginEmail").value;
         const password = document.getElementById("loginPassword").value;
 
-    
+       
         const passwordHash = btoa(password); 
+        const storedUser = JSON.parse(localStorage.getItem("user"));
 
-
-        const storedEmail = localStorage.getItem("email");
-        const storedPasswordHash = localStorage.getItem("passwordHash");
-
-        if (storedEmail && storedPasswordHash) {
-            
-            if (email === storedEmail && passwordHash === storedPasswordHash) {
+        if (storedUser) {
+            if (email === storedUser.email && passwordHash === storedUser.passwordHash) {
                 alert("Inicio de sesión exitoso");
-              
-                 window.location.href = "../index.html";
+           
+         window.location.href = "../index.html";
             } else {
                 alert("Correo electrónico o contraseña incorrectos");
             }
